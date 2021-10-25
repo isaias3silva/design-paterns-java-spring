022c9cf7b0614da6354f44d77edf2666276cf9cd
isaias3silva <52718665+isaias3silva@users.noreply.github.com>
2021-10-25 15:23:55 -0300

    Created class model: Cliente e endereco

--- src/main/java/com/mmlogistica/designpatternsjava/DesignPatternsJavaApplication.java
+++ src/main/java/com/mmlogistica/designpatternsjava/DesignPatternsJavaApplication.java
@@ -2,7 +2,20 @@ package com.mmlogistica.designpatternsjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+/**
+ * Projeto Spring Boot gerado via Spring Initializr.
+ * Os seguintes módulos foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ *
+ * @author isaias3silva
+ */
+
+@EnableFeignClients
 @SpringBootApplication
 public class DesignPatternsJavaApplication {
 
